402f0fd9b5f3103047eb84890b7c87797ded6ba8
Jessica <jessica.santosb@outlook.com>
2024-12-13 10:37:15 -0300

    feat: add user button to home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from '@clerk/nextjs';
 
-export default function Home() {
-  return <main>Home 
-    <Button variant={"destructive"}>enviar</Button>
-  </main>;
+export default function SetupPage() {
+  return (
+    <main>
+      <h2 className='p-4'>This is a protected route!</h2>
+      <UserButton />
+    </main>
+  );
 }
