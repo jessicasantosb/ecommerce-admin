1e9b73514d3ef136f28e5b919a1234fdd9618f6b
Jessica <jessica.santosb@outlook.com>
2024-12-14 15:41:29 -0300

    feat: use modal on home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,18 @@
-import { UserButton } from '@clerk/nextjs';
+'use client';
+
+import { useStoreModal } from '@/hooks/use-store-modal';
+import { useEffect } from 'react';
 
 export default function SetupPage() {
+  const { onOpen, isOpen } = useStoreModal((state) => state);
+
+  useEffect(() => {
+    if (!isOpen) onOpen();
+  }, [isOpen, onOpen]);
+
   return (
     <main>
-      <h2 className='p-4'>This is a protected route!</h2>
-      <UserButton />
+      <h2 className='p-4'>Route Page!</h2>
     </main>
   );
 }
