035536c5d8c46dba51b2c72d230fe783e1f9fd41
Jessica <jessica.santosb@outlook.com>
2024-12-17 19:00:23 -0300

    fix: send stores to store switcher

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,21 @@
 import { UserButton } from '@clerk/nextjs';
 
 import { Navbar } from '@/components/navbar';
+import { StoreSwitcher } from '@/components/store-switcher';
+import prisma from '@/lib/db';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
-export function Header() {
+export async function Header() {
+  const { userId } = await auth();
+  if (!userId) redirect('/sign-in');
+
+  if (!userId) redirect('/sign-in');
+  const stores = await prisma.store.findMany({ where: { userId } });
+
   return (
     <header className='h-16 flex items-center gap-2 px-4 mb-1 border-b'>
-      <div>store switcher</div>
+      <StoreSwitcher items={stores} />
       <Navbar />
       <div className='ml-auto flex items-center space-x-4'>
         <UserButton />
