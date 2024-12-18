9f5f5532163e8cecb7fc3de56586cd2918e4d498
Jessica <jessica.santosb@outlook.com>
2024-12-18 14:35:58 -0300

    feat: create update store name route

---
+++ app/api/stores/[storeId]/route.ts
@@ -0,0 +1,30 @@
+import prisma from '@/lib/db';
+import { auth } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string } },
+) {
+  try {
+    const { userId } = await auth();
+    const body = await req.json();
+
+    const name = body.values.name;
+
+    if (!userId) return new NextResponse('Unauthenticated', { status: 401 });
+    if (!name) return new NextResponse('Name is required', { status: 400 });
+    if (!params.storeId)
+      return new NextResponse('Store ID is required', { status: 400 });
+
+    const store = await prisma.store.updateMany({
+      where: { id: params.storeId, userId },
+      data: { name },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log('[STORES_PATCH]', error.message);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
