a7bd20202b5be0a9d55581bd42c2330b697813e9
Jessica <jessica.santosb@outlook.com>
2024-12-18 11:12:51 -0300

    feat: create store form field

---
+++ components/store.form-field.tsx
@@ -0,0 +1,43 @@
+import { Control } from 'react-hook-form';
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+import { Input } from './ui/input';
+
+interface StoreFormFieldProps {
+  control: Control<
+    {
+      name: string;
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    any
+  >;
+  isLoading: boolean;
+}
+
+export function StoreFormField({ control, isLoading }: StoreFormFieldProps) {
+  return (
+    <FormField
+      control={control}
+      name='name'
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Nome da loja</FormLabel>
+          <FormControl>
+            <Input
+              disabled={isLoading}
+              placeholder='Digite o nome'
+              {...field}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
