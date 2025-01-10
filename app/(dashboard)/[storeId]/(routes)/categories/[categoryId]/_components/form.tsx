a59ce46040267ac9311e8c81ddd748068e64f070
Jessica <jessica.santosb@outlook.com>
2025-01-10 09:54:43 -0300

    feat: create new category form

---
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/_components/form.tsx
@@ -0,0 +1,190 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Billboard, Category } from '@prisma/client';
+import axios from 'axios';
+import { Trash } from 'lucide-react';
+import { useParams, useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Heading } from '@/components/ui/heading';
+import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Separator } from '@/components/ui/separator';
+import { AlertModal } from '@/modals/alert-modal';
+import { categoryFormSchema } from '@/schemas/form-schema';
+
+interface CategoryFormProps {
+  initialData: Category | null;
+  billboards: Billboard[];
+}
+
+type CategoryFormValues = z.infer<typeof categoryFormSchema>;
+
+export function CategoryForm({ initialData, billboards }: CategoryFormProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const { storeId, categoryId } = useParams();
+  const { refresh, push } = useRouter();
+
+  const title = initialData ? 'Editar Categoria' : 'Criar Categoria';
+  const description = initialData
+    ? 'Editar uma Categoria'
+    : 'Adicionar nova Categoria';
+  const toastMessage = initialData
+    ? 'Categoria atualizada!'
+    : 'Categoria criada!';
+  const action = initialData ? 'Salvar mudanças' : 'Criar';
+
+  const form = useForm<CategoryFormValues>({
+    resolver: zodResolver(categoryFormSchema),
+    defaultValues: initialData || {
+      name: '',
+      billboardId: '',
+    },
+  });
+
+  const onSubmit = async (values: CategoryFormValues) => {
+    try {
+      setIsLoading(true);
+
+      if (initialData) {
+        await axios.patch(`/api/${storeId}/categories/${categoryId}`, {
+          values,
+        });
+      } else {
+        await axios.post(`/api/${storeId}/categories`, {
+          values,
+        });
+      }
+      push(`/${storeId}/categories`);
+      toast.success(toastMessage);
+    } catch (error) {
+      console.log(error);
+      toast.error('Algo deu errado! Tente novamente!');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`/api/${storeId}/categories/${categoryId}`);
+      refresh();
+      push(`/${storeId}/categories`);
+      toast.success('Categoria deletada!');
+    } catch (error) {
+      console.log(error);
+
+      toast.error('Primeiro remova todas os produtos dessa categoria.');
+    } finally {
+      setIsLoading(false);
+      setIsOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={isOpen}
+        isLoading={isLoading}
+        onClose={() => setIsOpen(false)}
+        onConfirm={onDelete}
+      />
+      <div className='flex items-center justify-between'>
+        <Heading title={title} description={description} />
+        {initialData && (
+          <Button
+            variant={'destructive'}
+            size={'icon'}
+            disabled={isLoading}
+            onClick={() => setIsOpen(true)}>
+            <Trash className='size-4' />
+          </Button>
+        )}
+      </div>
+
+      <Separator />
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='w-full space-y-8'>
+          <div className='grid grid-cols-2 gap-8'>
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder='Digite o nome'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name='billboardId'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Painel</FormLabel>
+                  <Select
+                    disabled={isLoading}
+                    onValueChange={field.onChange}
+                    value={field.value}
+                    defaultValue={field.value}>
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue
+                          defaultValue={field.value}
+                          placeholder='Selecione um painel'
+                        />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {billboards.map(({ id, label }) => (
+                        <SelectItem key={id} value={id}>
+                          {label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type='submit' disabled={isLoading} className='ml-auto'>
+            {action}
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+}
--- schemas/form-schema.ts
+++ schemas/form-schema.ts
@@ -8,3 +8,8 @@ export const billboardFormSchema = z.object({
   label: z.string().min(1, { message: 'O nome é obrigatório' }),
   imageUrl: z.string().min(1, { message: 'A imagem é obrigatória' }),
 });
+
+export const categoryFormSchema = z.object({
+  name: z.string().min(1, { message: 'O nome é obrigatório' }),
+  billboardId: z.string().min(1, { message: 'O id do painel é obrigatório' }),
+});
