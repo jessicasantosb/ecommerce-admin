d67b10d677f4f5b8fb87c1bc6cf0f5524eb129a6
Jessica <jessica.santosb@outlook.com>
2025-01-19 15:36:16 -0300

    feat: create size route

---
+++ app/api/[storeId]/sizes/[sizeId]/route.ts
@@ -0,0 +1,89 @@
+import prisma from '@/lib/db';
+import { auth } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+export async function GET(
+  _req: Request,
+  { params }: { params: { sizeId: string } },
+) {
+  try {
+    if (!params.sizeId)
+      return new NextResponse('Size id is required', { status: 400 });
+
+    const size = await prisma.size.findUnique({
+      where: { id: params.sizeId },
+    });
+
+    return NextResponse.json(size);
+  } catch (error) {
+    if (error instanceof Error) console.log('[SIZE_GET]', error.message);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string; sizeId: string } },
+) {
+  try {
+    const { userId } = await auth();
+    const body = await req.json();
+
+    const { name, value } = body.values;
+
+    if (!userId) return new NextResponse('Unauthenticated', { status: 401 });
+    if (!name) return new NextResponse('Name is required', { status: 400 });
+    if (!value) return new NextResponse('Value is required', { status: 400 });
+    if (!params.sizeId) {
+      return new NextResponse('Size id is required', { status: 400 });
+    }
+
+    const storeByUserId = await prisma.store.findFirst({
+      where: { id: params.storeId, userId },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse('Unauthorized', { status: 403 });
+    }
+
+    const size = await prisma.size.updateMany({
+      where: { id: params.sizeId },
+      data: { name, value },
+    });
+
+    return NextResponse.json(size);
+  } catch (error) {
+    if (error instanceof Error) console.log('[SIZE_PATCH]', error.message);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
+
+export async function DELETE(
+  _req: Request,
+  { params }: { params: { storeId: string; sizeId: string } },
+) {
+  try {
+    const { userId } = await auth();
+
+    if (!userId) return new NextResponse('Unauthenticated', { status: 401 });
+    if (!params.sizeId)
+      return new NextResponse('Size id is required', { status: 400 });
+
+    const storeByUserId = await prisma.store.findFirst({
+      where: { id: params.storeId, userId },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse('Unauthorized', { status: 403 });
+    }
+
+    const size = await prisma.size.deleteMany({
+      where: { id: params.sizeId },
+    });
+
+    return NextResponse.json(size);
+  } catch (error) {
+    if (error instanceof Error) console.log('[SIZE_DELETE]', error.message);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
