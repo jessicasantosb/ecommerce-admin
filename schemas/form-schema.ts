f19c5b6550ecd360c6005403bbebf024b802992c
Jessica <jessica.santosb@outlook.com>
2025-01-19 09:51:25 -0300

    feat: create size form schema

--- schemas/form-schema.ts
+++ schemas/form-schema.ts
@@ -13,3 +13,9 @@ export const categoryFormSchema = z.object({
   name: z.string().min(1, { message: 'O nome é obrigatório' }),
   billboardId: z.string().min(1, { message: 'O id do painel é obrigatório' }),
 });
+
+export const sizeFormSchema = z.object({
+  name: z.string().min(1, { message: 'O nome é obrigatório' }),
+  value: z.string().min(1, { message: 'O tamanho é obrigatório' }),
+  sizeId: z.string().min(1, { message: 'O id do tamanho é obrigatório' }),
+});
