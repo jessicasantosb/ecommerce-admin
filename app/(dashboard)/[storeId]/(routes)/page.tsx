1bfcfe7b880616fe651d77fc6a468efb409812f5
Jessica <jessica.santosb@outlook.com>
2025-03-14 13:56:24 -0300

    feat: add cards to dashboard

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,8 +1,56 @@
-export default function DashboardPage() {
+import { CreditCard, DollarSign } from 'lucide-react';
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Heading } from '@/components/ui/heading';
+import { Separator } from '@/components/ui/separator';
+import { priceFormatter } from '@/lib/utils';
+
+export default async function DashboardPage() {
 
   return (
-    <main>
-      <h3>DashboardPage</h3>
+    <main className='flex items-center'>
+      <div className='flex-1 p-8 pt-6 space-y-4'>
+        <Heading title='Dashboard' description='Visão geral da sua loja' />
+        <Separator />
+
+        <div className='grid gap-4 grid-cols-3'>
+          <Card>
+            <CardHeader className='pb-2 space-y-0 flex flex-row items-center justify-between'>
+              <CardTitle className='text-sm font-medium'>
+                Total recebido
+              </CardTitle>
+              <DollarSign className='size-4 text-muted-foreground' />
+            </CardHeader>
+            <CardContent>
+              <div className='text-2xl font-bold'>
+                {priceFormatter.format(10)}
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader className='pb-2 space-y-0 flex flex-row items-center justify-between'>
+              <CardTitle className='text-sm font-medium'>Vendas</CardTitle>
+              <CreditCard className='size-4 text-muted-foreground' />
+            </CardHeader>
+            <CardContent>
+              <div className='text-2xl font-bold'>+25</div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader className='pb-2 space-y-0 flex flex-row items-center justify-between'>
+              <CardTitle className='text-sm font-medium'>
+                Produtos em estoque
+              </CardTitle>
+              <CreditCard className='size-4 text-muted-foreground' />
+            </CardHeader>
+            <CardContent>
+              <div className='text-2xl font-bold'>200</div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
     </main>
   );
 }
