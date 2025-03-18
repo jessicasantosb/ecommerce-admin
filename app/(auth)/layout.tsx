43f54e4e84582a80107f0b16cfd0857c0ed8c573
Jessica <jessica.santosb@outlook.com>
2025-03-18 12:48:29 -0300

    style: center login/register pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,5 @@
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 
-export default function AuthLayout({children} : {children: ReactNode}) {
-
-  return (
-    <main className="container flex items-center justify-center py-14">
-      {children}
-    </main>
-  );
+export default function AuthLayout({ children }: { children: ReactNode }) {
+  return <main className='size-full grid place-items-center'>{children}</main>;
 }
