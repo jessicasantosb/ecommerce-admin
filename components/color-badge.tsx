708972b7a1a4aaea412ae093f4563057ff5f4ab7
Jessica <jessica.santosb@outlook.com>
2025-03-27 13:27:04 -0300

    style: invert color badge appearance

--- components/color-badge.tsx
+++ components/color-badge.tsx
@@ -5,11 +5,11 @@ type ColorBadgeProps = {
 export function ColorBadge({ value }: ColorBadgeProps) {
   return (
     <div className='flex items-center gap-x-2'>
-      {value}
       <div
         className='size-6 rounded-full border'
         style={{ backgroundColor: value }}
       />
+      {value}
     </div>
   );
 }
